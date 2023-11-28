f94ca14b58b264ebed0cb127b477a06ceab80803
Isobel-Weaver <148205155+Isobel-Weaver@users.noreply.github.com>
2023-11-28 17:14:57 +0000

    Add files via upload
    Controllers and views to show and edit customer details.

---
+++ showDetails.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<table>
+<?php
+    echo '<tr><td>Username ' . $user->username . '</td></tr><tr><td>First Name '. $user->firstname .'<tr><td>Surname ' . $user->surname . '</td></tr> <tr><td>Address ' . $user->address . '</td></tr> <tr><td>Email ' . $user->email . '</td></tr><tr><td>Phone Number ' . $user->phone . '</td></tr>';
+?>
+</table>
+</body>
+</html>
