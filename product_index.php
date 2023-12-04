6f9ba17914fdc09f278825cffaa778d8a1765288
Luke-H-220095075 <148209315+Luke-H-220095075@users.noreply.github.com>
2023-12-04 23:37:20 +0000

    Add files via upload

--- product_index.php
+++ product_index.php
@@ -11,14 +11,104 @@
 $dsn = "mysql:host=localhost;dbname=furniche";
 $username = 'root';
 $password = '';
+?>
+
+<h2>Products</h2>
+
+<!-- Add dropdown filters -->
+<label for="categoryFilter">Filter by Category:</label>
+<select id="categoryFilter" onchange="filterProducts()">
+    <option value="all">All</option>
+    <?php
+    $selectedcategoryFilter = isset($_GET['categoryFilter']) ? $_GET['categoryFilter'] : 'all';
+
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $pdo->query("SELECT DISTINCT productCategory FROM products");
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $category = htmlspecialchars($row['productCategory']);
+            $selected = ($category == $selectedcategoryFilter) ? 'selected' : '';
+            if ($category != null) {
+                echo '<option value="' . $category . '" ' . $selected . '>' . $category . '</option>';
+            }
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    $pdo = null;
+    ?>
+</select>
+
+<label for="typeFilter">Filter by Type:</label>
+<select id="typeFilter" onchange="filterProducts()">
+    <option value="all">All</option>
+    <?php
+    $selectedtypeFilter = isset($_GET['typeFilter']) ? $_GET['typeFilter'] : 'all';
+
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $pdo->query("SELECT DISTINCT productType FROM products");
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $type = htmlspecialchars($row['productType']);
+            $selected = ($type == $selectedtypeFilter) ? 'selected' : '';
+            if ($type != null) {
+                echo '<option value="' . $type . '" ' . $selected . '>' . $type . '</option>';
+            }
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    $pdo = null;
+    ?>
+</select>
+
+<button onclick="resetFilters()">Reset Filters</button>
+
+<?php
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->query("SELECT * FROM products");
+    // Initial query to fetch all products
+    $query = "SELECT * FROM products";
+
+    // Fetch selected values from dropdowns
+    $typeFilter = isset($_GET['typeFilter']) ? $_GET['typeFilter'] : 'all';
+    $categoryFilter = isset($_GET['categoryFilter']) ? $_GET['categoryFilter'] : 'all';
+
+    // Add conditions to the query based on selected filters
+    if ($typeFilter != 'all') {
+        $query .= " WHERE productType = :type";
+    }
+
+    if ($categoryFilter != 'all') {
+        $query .= ($typeFilter != 'all') ? " AND" : " WHERE";
+        $query .= " productCategory = :category";
+    }
+
+    // Prepare and execute the final query
+    $stmt = $pdo->prepare($query);
+
+    if ($typeFilter != 'all') {
+        $stmt->bindParam(':type', $typeFilter, PDO::PARAM_STR);
+    }
+
+    if ($categoryFilter != 'all') {
+        $stmt->bindParam(':category', $categoryFilter, PDO::PARAM_STR);
+    }
+
+    $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
-        echo '<table>';
+        echo '<table id="productTable">';
         $count = 0; // Counter for products in the current row
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -44,7 +134,7 @@ try {
 
         echo '</table>';
     } else {
-        echo "<p>No products available</p>";
+        echo "<p>No products available, add some to the database</p>";
     }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
@@ -52,6 +142,7 @@ try {
 
 $pdo = null;
 ?>
+
 <div id="productModal" class="modal">
     <div class="modal-content">
         <span class="close" onclick="closeProductModal()">&times;</span>
@@ -89,6 +180,19 @@ $pdo = null;
             modal.style.display = 'none';
         }
     };
+
+    function filterProducts() {
+        var typeFilter = document.getElementById("typeFilter").value;
+        var categoryFilter = document.getElementById("categoryFilter").value;
+
+        // Redirect to the same page with the selected filters as query parameters
+        window.location.href = 'product_index.php?typeFilter=' + typeFilter + '&categoryFilter=' + categoryFilter;
+    }
+
+    function resetFilters() {
+        // Redirect to the same page without any filters
+        window.location.href = 'product_index.php';
+    }
 </script>
 </body>
 </html>
