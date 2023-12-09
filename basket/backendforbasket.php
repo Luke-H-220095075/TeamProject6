4084ee916445c5e405e99bbdd3c70d1b96fe6300
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-09 21:50:23 +0000

    error fixes

--- basket/backendforbasket.php
+++ basket/backendforbasket.php
@@ -2,20 +2,19 @@
 include 'connectdb.php';
       $sql = "SELECT productId FROM basketproducts WHERE basketId = $basket_id";
       $result = $conn->query($sql);
-      $basketcost = 0
-      if ($result->rowCount() > 0) {
+      $basketcost = 0;
+      if($result->rowCount() > 0) {
         while($row = $result->fetch()) {
-            $sql = "SELECT price FROM products WHERE $result = $row["productId"]";
+            $sql = "SELECT price FROM products WHERE $result = $row[productId]";
             $productprice = $conn->query($sql);
-            $basketcost = $basketcost + $productprice
-          
-        }
+            $basketcost = $basketcost + $productpricell;
+          }
       echo "£" . $basketcost . " before discount</br>";
       } else {
         echo "0 results";
       }
-      $discount_name = $_POST['discount']
-      $sql = "SELECT 'value' FROM discounts WHERE $discount_name = $row["discountTitle"]";
+      $discount_name = $_POST['discount'];
+      $sql = "SELECT 'value' FROM discounts WHERE $discount_name = $row[discountTitle]";
       $value = $conn->query($sql);
       $basketcost = $basketcost * 100 - $value
     ?>
