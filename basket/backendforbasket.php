7c244eb688ee2b172137363f502f7735e91d80b8
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-09 23:28:52 +0000

    removed errors
    certain values are placeholders until connected to frontend

--- basket/backendforbasket.php
+++ basket/backendforbasket.php
@@ -1,20 +1,23 @@
 <?php
 include 'connectdb.php';
-      $sql = "SELECT productId FROM basketproducts WHERE basketId = $basket_id";
+$basket_id = 1;
+      $sql = "SELECT price FROM products join basketproducts ON products.productId = basketproducts.productId WHERE basketId = $basket_id";
       $result = $conn->query($sql);
       $basketcost = 0;
       if($result->rowCount() > 0) {
         while($row = $result->fetch()) {
-            $sql = "SELECT price FROM products WHERE $result = $row[productId]";
-            $productprice = $conn->query($sql);
-            $basketcost = $basketcost + $productpricell;
+            $basketcost = $basketcost + $row["price"];
           }
       echo "£" . $basketcost . " before discount</br>";
       } else {
         echo "0 results";
       }
-      $discount_name = $_POST['discount'];
-      $sql = "SELECT 'value' FROM discounts WHERE $discount_name = $row[discountTitle]";
+
+
+      
+      $discount_name = "Discount 1";#$discount_name = $_POST['discount'];
+      $sql = "SELECT value FROM discounts WHERE discountTitle = '" . $discount_name."'"; 
       $value = $conn->query($sql);
-      $basketcost = $basketcost * 100 - $value
+      $basketcost = $basketcost * (1 - $value->fetch()["value"] / 100);
+      echo "£". $basketcost . " total";
     ?>
