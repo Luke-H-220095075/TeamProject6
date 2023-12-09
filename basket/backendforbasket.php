1b9f09570d1a53c9be93e9a73c43ccb568f51499
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-09 20:49:44 +0000

    added basket price and discount calculation
    ignore last commit title autosave was off and therefore it was mistitled this is what it was meant to commit plus discount calculations

--- basket/backendforbasket.php
+++ basket/backendforbasket.php
@@ -1,16 +1,21 @@
 <?php
 include 'connectdb.php';
-      $sql = "SELECT * FROM basketproducts WHERE basketId = $basket_id";
+      $sql = "SELECT productId FROM basketproducts WHERE basketId = $basket_id";
       $result = $conn->query($sql);
-      
+      $basketcost = 0
       if ($result->rowCount() > 0) {
         while($row = $result->fetch()) {
-            $sql = "SELECT * FROM products WHERE $row["basketId"] = $row["productId"]";
-            $productresult = $conn->query($sql);
-          echo $row["imageName"] . "</br>";
+            $sql = "SELECT price FROM products WHERE $result = $row["productId"]";
+            $productprice = $conn->query($sql);
+            $basketcost = $basketcost + $productprice
+          
         }
+      echo "£" . $basketcost . " before discount</br>";
       } else {
         echo "0 results";
       }
-    
+      $discount_name = $_POST['discount']
+      $sql = "SELECT 'value' FROM discounts WHERE $discount_name = $row["discountTitle"]";
+      $value = $conn->query($sql);
+      $basketcost = $basketcost * 100 - $value
     ?>
