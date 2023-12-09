1a58bf788746fa8c9e5d7200d8e4e12a2cc32297
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-09 19:48:40 +0000

    corrected database connection

--- connectdb.php
+++ connectdb.php
@@ -2,10 +2,11 @@
 
 $db_host = 'localhost';
 $db_name = 'u_220226381_db';
-$username = 'u-220226381';
-$password = 'inRuQ3XSct6NMd1';
+$dsn = "mysql:host=localhost;dbname=furniche";
+$username = "root";
+$password = "";
 try {
-    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $username, $password);
+    $conn = new PDO($dsn, $username, $password);
 	#$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $ex) {
 	echo("Failed to connect to the database.<br>");
