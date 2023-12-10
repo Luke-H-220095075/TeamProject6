77c8e749e154ab7c232909f6981caf7c83c81101
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-10 05:44:07 +0000

    added stock changing of purchasing
    also removed debugging artefact and autoformatted

--- basket/backendforbasket.php
+++ basket/backendforbasket.php
@@ -1,43 +1,63 @@
+<html>
 <?php
 include 'connectdb.php';
 $basket_id = 1;
-      $sql = "SELECT price, quantity FROM products JOIN basketproducts ON products.productId = basketproducts.productId WHERE basketId = $basket_id";
-      $result = $conn->query($sql);
-      $basketcost = 0;
-      if($result->rowCount() > 0) {
-        while($row = $result->fetch()) {
-            $basketcost = $basketcost + $row["quantity"] * $row["price"];
-        }
-      echo "£" . $basketcost . " before discount</br>";
-      } else {
-        echo "0 results";
-      }
+$sql = "SELECT price, quantity FROM products JOIN basketproducts ON products.productId = basketproducts.productId WHERE basketId = $basket_id";
+$result = $conn->query($sql);
+$basketcost = 0;
+if ($result->rowCount() > 0) {
+  while ($row = $result->fetch()) {
+    $basketcost = $basketcost + $row["quantity"] * $row["price"];
+  }
+  echo "£" . $basketcost . " before discount</br>";
+} else {
+  echo "0 results";
+}
 
 
-      
-      $discount_name = "Discount 1";#$discount_name = $_POST['discount'];
-      $sql = "SELECT value FROM discounts WHERE discountTitle = '" . $discount_name."'"; 
-      $value = $conn->query($sql);
-      $basketcost = $basketcost * (1 - $value->fetch()["value"] / 100);
-      echo "£". $basketcost . " total</br>";
+
+$discount_name = "Discount 1"; #$discount_name = $_POST['discount'];
+$sql = "SELECT value FROM discounts WHERE discountTitle = '" . $discount_name . "'";
+$value = $conn->query($sql);
+$basketcost = $basketcost * (1 - $value->fetch()["value"] / 100);
+echo "£" . $basketcost . " total</br>";
 
 
-      #stock availability check
-      function availability($conn){
-      $available = true;
-      $sql = "SELECT productName, countStock, quantity FROM products join basketproducts ON products.productId = basketproducts.productId  WHERE basketId = $basket_id";
-      $result = $conn->query($sql);
-      if($result->rowCount() > 0) {
-        while($row = $result->fetch()) {
-          if($row["quantity"] + 100 > $row["countStock"]){
-            echo $row["productName"] . " is unavailable </br>";
-            $available = false;
-          }
-        }
-      }
-      if ($available) { 
-        echo "available";
+#stock availability check
+function availability($conn, $basket_id)
+{
+  $available = true;
+  $sql = "SELECT productName, countStock, quantity FROM products join basketproducts ON products.productId = basketproducts.productId  WHERE basketId = $basket_id";
+  $result = $conn->query($sql);
+  if ($result->rowCount() > 0) {
+    while ($row = $result->fetch()) {
+      if ($row["quantity"] > $row["countStock"]) {
+        echo $row["productName"] . " is unavailable </br>";
+        $available = false;
       }
     }
-    availability($conn)
+  }
+  return $available;
+}
+if (availability($conn, $basket_id)) {
+  echo "available";
+}
+
+if (isset($_POST['purchase'])) {
+  if (availability($conn, $basket_id)) {
+    $sql = "SELECT countStock, countSold, quantity, productId FROM products join basketproducts ON products.productId = basketproducts.productId  WHERE basketId = $basket_id";
+    $result = $conn->query($sql);
+    if ($result->rowCount() > 0) {
+      while ($row = $result->fetch()) {
+        $sql = "UPDATE products SET countStock = " . $row["countStock"] - $row["quantity"] . ", countSold = " . $row["countSold"] + $row["quantity"] . " WHERE productId = " . $row["productId"];
+        $conn->query($sql);
+      }
+    }
+  }
+}
+
 ?>
+<form method="post"> 
+        <input type="submit" name="purchase" value="purchase"/>
+</form> 
+</html>
