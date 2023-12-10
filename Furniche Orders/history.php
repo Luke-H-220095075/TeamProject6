ee24334beabddd9d7f54345d2b532fa9c4a3a98d
LuckyMuse <luckymuse2001@gmail.com>
2023-12-10 14:21:50 +0000

    Created History.php
    Creation of new page and database connection.
    
    Confusion between different repositories with the same name, the code will moved to this one for the team project.

---
+++ Furniche Orders/history.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order History</title>
+    <link rel="stylesheet" href="styles.css">
+</head> 
+</head>
+<body>
+
+<strong><h2>Order History</h2></strong>
+
+<?php
+$dsn = "mysql:host=localhost;dbname=furniche";
+$username = "root";
+$password = "";
