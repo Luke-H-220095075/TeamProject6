56392aa1bcaa743bceedbed96c2122a4376d2f40
LuckyMuse <luckymuse2001@gmail.com>
2023-12-10 14:34:10 +0000

    Update history.php
    Allows users to sort their previous orders by newest, oldest and those that have no been delivered.
    
    Confusion between different repositories with the same name, the code will moved to this one for the team project.

--- Furniche Orders/history.php
+++ Furniche Orders/history.php
@@ -15,3 +15,34 @@
 $dsn = "mysql:host=localhost;dbname=furniche";
 $username = "root";
 $password = "";
+
+//Data Base connection 
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Test with user 1 change once login page is connected
+    $userID = 1;
+    $ordersPerPage = 6; // limit to six order viewed per page
+
+    // Sort the previous orders by newest and oldest dates ordered and those that have no been delivered yet
+    $sortOption = isset($_GET['sort']) ? $_GET['sort'] : 'newest_order';
+    $sorting = '';
+    switch ($sortOption) {
+        case 'newest_order':
+            $sorting = 'ORDER BY o.dateAdded DESC';
+            break;
+        case 'oldest_order':
+            $sorting = 'ORDER BY o.dateAdded ASC';
+            break;
+        case 'not_delivered':
+            $sorting = 'ORDER BY COALESCE(o.deliveryDate > CURDATE(), 0) DESC, o.deliveryDate ASC';
+            break;
+        default:
+            $sorting = 'ORDER BY o.dateAdded DESC';
+            break;
+    }
+
+    $page = isset($_GET['page']) ? max(1, intval($_GET['page'])) : 1;
+    $offset = ($page - 1) * $ordersPerPage;
