f932f8702cd0cbcc00360265ff534ea8f72dd52f
t00000mmmmm <148207374+t00000mmmmm@users.noreply.github.com>
2023-12-11 05:33:54 +0000

    moved some backend for the basket into the basket.php file

--- basket.php
+++ basket.php
@@ -58,8 +58,50 @@ try {
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-
+echo "<br>";
 $pdo = null;
+include 'connectdb.php';
+$basket_id = 1;
+$sql = "SELECT price, quantity FROM products JOIN basketproducts ON products.productId = basketproducts.productId WHERE basketId = $basket_id";
+$result = $conn->query($sql);
+$basketcost = 0;
+if ($result->rowCount() > 0) {
+  while ($row = $result->fetch()) {
+    $basketcost = $basketcost + $row["quantity"] * $row["price"];
+  }
+  echo "£" . $basketcost . " before discount</br>";
+} else {
+  echo "0 results";
+}
+
+
+
+$discount_name = "Discount 1"; #$discount_name = $_POST['discount'];
+$sql = "SELECT value FROM discounts WHERE discountTitle = '" . $discount_name . "'";
+$value = $conn->query($sql);
+$basketcost = $basketcost * (1 - $value->fetch()["value"] / 100);
+echo "£" . $basketcost . " total</br>";
+
+
+#stock availability check
+function availability($conn, $basket_id)
+{
+  $available = true;
+  $sql = "SELECT productName, countStock, quantity FROM products join basketproducts ON products.productId = basketproducts.productId  WHERE basketId = $basket_id";
+  $result = $conn->query($sql);
+  if ($result->rowCount() > 0) {
+    while ($row = $result->fetch()) {
+      if ($row["quantity"] > $row["countStock"]) {
+        echo $row["productName"] . " is unavailable </br>";
+        $available = false;
+      }
+    }
+  }
+  return $available;
+}
+if (availability($conn, $basket_id)) {
+  echo "available";
+}
 ?>
 
 <script>
