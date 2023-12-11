77465784a7979bf669b2567ee3f79cf2497d2e91
Luke-H-220095075 <148209315+Luke-H-220095075@users.noreply.github.com>
2023-12-11 16:58:06 +0000

    php and js for pages

---
+++ loginview.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form Design</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    
+    <div class="container">
+
+        <div class="image_box">
+            <img src="Pictures%20for%20website/loginpic.jpg">
+        </div>
+
+        <div class="box">
+            <h2>Login</h2>
+            <form method="post">
+
+                <div class="input_box">
+                    <input type="text" name="username" required>
+                    <label>Username</label>
+                </div>
+
+                <div class="input_box">
+                    <input type="password" name="password" required>
+                    <label>Password</label>
+                </div>
+                <script src="showPasswordScript.js"></script>
+                <label class="checkbox"><input type="checkbox" name="passcbx" onclick="showPassword()"><p>Show Password</p></label>
+
+                <div class="forgot_password_box">
+                    <div class="forgot_password"><a href="#">Forgot Password?</a></div>
+                </div>
+
+                <div class="link_box">
+                    <div class="link">By creating an account you agree to <a href="#">Terms & Conditions</a></div>
+                </div>
+
+                <button class="login_button" name="submitted" type="submit">Login</button>
+                <?php
+                include("signIn.php");
+                ?>
+
+                <div class="link_box">
+                    <div class="signup_link">Don't have an account? <a href="sign-up.php">Sign Up</a></div>
+                    <div class="contact_link">Need Help? <a href="contact.php">Contact Us</a></div>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+
+</body>
+</html>
---
+++ product_show.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Details</title>
+    <link rel="stylesheet" href="css/product.css">
+</head>
+<body>
+<?php
+$dsn = "mysql:host=localhost;dbname=furniche";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (isset($_GET['product_id'])) {
+        $productId = $_GET['product_id'];
+
+        $stmt = $pdo->prepare("SELECT * FROM products WHERE productId = ?");
+        $stmt->execute([$productId]);
+
+        if ($stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            echo '<h3>Product Details</h3>';
+            echo '<p><strong>Name:</strong> ' . htmlspecialchars($row['productName']) . '</p>';
+            echo '<p><strong>Price:</strong> $' . number_format($row['price'], 2) . '</p>';
+            echo '<p><img src="Pictures%20for%20website/' . htmlspecialchars($row['imageName']) . '" alt="' . htmlspecialchars($row['imageName']) . '" class="modal-image"></p>';
+        } else {
+            echo '<p>No product found with the given ID</p>';
+        }
+    } else {
+        echo '<p>Product ID not provided</p>';
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
+?>
+</body>
+</html>
