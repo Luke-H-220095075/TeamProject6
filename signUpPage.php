a5f711e3d6a3d06b9a09e2d8ea37e3b403a339de
Luke-H-220095075 <220095075@aston.ac.uk>
2024-02-14 09:43:12 +0000

    title change

--- signUpPage.php
+++ signUpPage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form Design</title>
+    <title>Furniche - Sign Up</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
