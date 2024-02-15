5027ff2b2968e0c2114d5a693243c7457bc852a9
Isobel-Weaver <148205155+Isobel-Weaver@users.noreply.github.com>
2024-02-15 14:52:56 +0000

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -12,7 +12,7 @@
              include_once("User.php");                                                               //($username, $password, $email, $firstname, $surname, $address, $phone, $admin)
               $user = new User($_POST['username'], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST['email'], $_POST['firstname'], $_POST['surname'], null, null, 'Customer');
         include_once("SignUpController.php");
-        $controller = new SignUpController($user);
+        $controller = new SignUpController($user, password_hash($_POST['sanswer']));
         $controller->invoke();
             }
             else{
