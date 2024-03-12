b11601bb307aabae904317ca6f5c73dbab0080c7
Shayaan Hussain <220140560@aston.ac.uk>
2024-03-12 17:26:19 +0000

    Made a new history page

--- About Us.php
+++ About Us.php
@@ -74,8 +74,7 @@
     <!-- Mission and Story Cards -->
     <div class="content-container">
         <div class="card" id="our-story">
-            <img src="https://c0.wallpaperflare.com/preview/11/747/606/aerial-shot-bird-s-eye-view-dark-green-daylight.jpg"
-                alt="Our Story Image">
+            <img src="https://c0.wallpaperflare.com/preview/11/747/606/aerial-shot-bird-s-eye-view-dark-green-daylight.jpg" alt="Our Story Image">
             <div class="text-content">
                 <h2>Our Story</h2>
                 <p>Launched in 2000, based off finding ethically sourced wooden furniture for all types of people. We
@@ -83,8 +82,7 @@
             </div>
         </div>
         <div class="card" id="mission-statement">
-            <img src="https://images.freeimages.com/images/large-previews/c28/forest-sunrise-1334131.jpg"
-                alt="Mission Statement Image">
+            <img src="https://images.freeimages.com/images/large-previews/c28/forest-sunrise-1334131.jpg" alt="Mission Statement Image">
             <div class="text-content">
                 <h2>Mission Statement</h2>
                 <p>To ethically source stylish wood for every niche and replant the wood taken. We replant 10 more trees
@@ -133,10 +131,7 @@
     <br><br>
     <!-- Map -->
     <section class="locaction">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.6980768196954!2d-0.08362608488829902!3d51.51875491769485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876034c44354bb1%3A0xc57f7d6c0b61ff5f!2sLiverpool%20Street!5e0!3m2!1sen!2suk!4v1677666062080!5m2!1sen!2suk"
-            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.6980768196954!2d-0.08362608488829902!3d51.51875491769485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876034c44354bb1%3A0xc57f7d6c0b61ff5f!2sLiverpool%20Street!5e0!3m2!1sen!2suk!4v1677666062080!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <span>
             <h3>Our Locations</h3>
             <br>
@@ -148,47 +143,42 @@
         </span>
     </section>
 
-
-
-
-
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="footer-col">
-                <h4>About Us</h4>
-               <ul>
-                <li><a href="#">Our Founder</a> </li>
-                <li><a href="#">Our Values</a> </li>
-                <li><a href="#">Our Privacy Policy</a> </li>
-                <li><a href="#">Our Services</a> </li>
-            </ul>
-            </div>
-            <div class="footer-col">
-                <h4>Address</h4>
-                <h5>206 Canada Place, Liverpool Street, E12 1CL</h5>
-            </div>
-            <div class="footer-col">
-                <h4>Contact Us</h4>
-                <h5>Email us at: comms@furniche.com</h5>
-                <h5>Call us at: 01563385967</h5>
-                <ul>
-                    <li><a href="contactview.php">Contact Us via our Website</a> </li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4>Follow us</h4>
-                <div class="social-links">
-                    <a href="https://en-gb.facebook.com/"><i class="fab fa-facebook - f"></i></a>
-                    <a href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
-                    <a href="https://uk.linkedin.com/"><i class="fab fa-linkedin - in"></i></a>
-                    <a href="https://github.com/"><i class="fab fa-github"></i></a>
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-                  </div>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>About Us</h4>
+                    <ul>
+                        <li><a href="#">Our Founder</a> </li>
+                        <li><a href="#">Our Values</a> </li>
+                        <li><a href="#">Our Privacy Policy</a> </li>
+                        <li><a href="#">Our Services</a> </li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Address</h4>
+                    <h5>206 Canada Place, Liverpool Street, E12 1CL</h5>
+                </div>
+                <div class="footer-col">
+                    <h4>Contact Us</h4>
+                    <h5>Email us at: comms@furniche.com</h5>
+                    <h5>Call us at: 01563385967</h5>
+                    <ul>
+                        <li><a href="contactview.php">Contact Us via our Website</a> </li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Follow us</h4>
+                    <div class="social-links">
+                        <a href="https://en-gb.facebook.com/"><i class="fab fa-facebook - f"></i></a>
+                        <a href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
+                        <a href="https://uk.linkedin.com/"><i class="fab fa-linkedin - in"></i></a>
+                        <a href="https://github.com/"><i class="fab fa-github"></i></a>
+                        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 </body>
-
 </html>
--- changePassword.php
+++ changePassword.php
@@ -6,28 +6,30 @@
     <link rel="stylesheet" href="css/customerprofile.css">
 </head>
 <div class="colour">
-  <header>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-      <link rel="stylesheet" href="https://use.typekit.net/maf1fpm.css">
-  </a>
+    <header>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <link rel="stylesheet" href="https://use.typekit.net/maf1fpm.css">
+        </a>
 </div>
 <section>
-  <div class="topnav">
-      <nav>
-          <h1 class="logo">Furniche</h1>
-          <ul>
-              <li><a class="active" href="Main.html">Home</a></li>
-              <li><a href="loginview.php">Login</a></li>
-              <li><a href="contactview.php">Contact Us</a></li>
-              <li><a href="aboutus.php">About Us</a></li>
-          </ul>
-  </nav>
-  </div>
+    <div class="topnav">
+        <nav>
+            <h1 class="logo">Furniche</h1>
+            <ul>
+                <li><a class="active" href="Main.html">Home</a></li>
+                <li><a href="loginview.php">Login</a></li>
+                <li><a href="contactview.php">Contact Us</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+            </ul>
+        </nav>
+    </div>
 </section>
 </header>
 </div>
-<body><?php
-require("../../../TeamProject6/model/User.php");?>
+
+<body>
+    <?php
+    require("../../../TeamProject6/model/User.php"); ?>
     <section>
         <div class="profile">
             <img src="Thomas.jpg" alt="My Image" class="my_img">
@@ -37,61 +39,60 @@ require("../../../TeamProject6/model/User.php");?>
                     //$user= new User(null, null, null, null, null, null, null, null);
                     $user->getDetails();
                     echo '
-                    <input type="firstname"name="firstname"placeholder="Mothers Maiden Name " value='.$user->firstname.' required/>
+                    <input type="firstname"name="firstname"placeholder="Mothers Maiden Name " value=' . $user->firstname . ' required/>
                     <br>
-                    <input type="password"name="password"placeholder="New Password" value='.$user->surname.' required/>
+                    <input type="password"name="password"placeholder="New Password" value=' . $user->surname . ' required/>
                     <br>
                     <button type="submit" name="submitted" id="submit-btn">Change Password</button>
                 </form>';
-                if(isset($_POST["submitted"])) 
-  {
-      //$user = new User(null, null, $_POST['email'], $_POST['firstname'], $_POST['surname'], $_POST['address'], $_POST['phone'], null);
-      include_once("controller/UpdateDetailsController.php");
-      //$controller = new UpdateDetailsController($user);
-      $controller->invoke();
-      //header('Location: update.php');
-  }?>
+                    if (isset($_POST["submitted"])) {
+                        //$user = new User(null, null, $_POST['email'], $_POST['firstname'], $_POST['surname'], $_POST['address'], $_POST['phone'], null);
+                        include_once("controller/UpdateDetailsController.php");
+                        //$controller = new UpdateDetailsController($user);
+                        $controller->invoke();
+                        //header('Location: update.php');
+                    } ?>
             </div>
         </div>
     </section>
 </body>
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="footer-col">
-                    <h4>About Us</h4>
-                   <ul>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>About Us</h4>
+                <ul>
                     <li><a href="#">Our Founder</a> </li>
                     <li><a href="#">Our Values</a> </li>
                     <li><a href="#">Our Privacy Policy</a> </li>
                     <li><a href="#">Our Services</a> </li>
                 </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>Address</h4>
-                    <h5>206 Canada Place, Liverpool Street, E12 1CL</h5>
-                </div>
-                <div class="footer-col">
-                    <h4>Contact Us</h4>
-                    <h5>Email us at: comms@furniche.com</h5>
-                    <h5>Call us at: 01563385967</h5>
-                    <ul>
-                        <li><a href="contactview.php">Contact Us via our Website</a> </li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>Follow us</h4>
-                    <div class="social-links">
-                        <a href="https://en-gb.facebook.com/"><i class="fab fa-facebook - f"></i></a>
-                        <a href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
-                        <a href="https://uk.linkedin.com/"><i class="fab fa-linkedin - in"></i></a>
-                        <a href="https://github.com/"><i class="fab fa-github"></i></a>
-                        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-                      </div>
+            </div>
+            <div class="footer-col">
+                <h4>Address</h4>
+                <h5>206 Canada Place, Liverpool Street, E12 1CL</h5>
+            </div>
+            <div class="footer-col">
+                <h4>Contact Us</h4>
+                <h5>Email us at: comms@furniche.com</h5>
+                <h5>Call us at: 01563385967</h5>
+                <ul>
+                    <li><a href="contactview.php">Contact Us via our Website</a> </li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Follow us</h4>
+                <div class="social-links">
+                    <a href="https://en-gb.facebook.com/"><i class="fab fa-facebook - f"></i></a>
+                    <a href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
+                    <a href="https://uk.linkedin.com/"><i class="fab fa-linkedin - in"></i></a>
+                    <a href="https://github.com/"><i class="fab fa-github"></i></a>
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
         </div>
-    </footer>
-    </body>
-    
-    </html>
+    </div>
+</footer>
+</body>
+
+</html>
