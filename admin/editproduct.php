a0ff527233a76ae9e88c3956cef3601b7f108c4a
LuckyMuse <luckymuse2001@gmail.com>
2024-03-19 15:06:21 +0000

    Created editproduct.php
    New file created. Header and Side bar were added.

---
+++ admin/editproduct.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/d4aa4c134e.js" crossorigin="anonymous"></script>
+    <title>FurnicheDashboard</title>
+</head>
+
+<header>
+<span>Furniche <i class="fa-solid fa-bars" id="togglebtn"></i></span>
+
+<?php
+include '../connect.php';
+
+
+session_start();
+
+        ?>
+        </div>
+        </header>
+<body>
+<section id="sidebar">
+        <ul class="side-menu top">
+            <div class="logo">
+            </div>
+           
+            <li class="active">
+            <?php
+
+if (isset($_SESSION['user'])) {
+    echo '
+    <div class="user-info">
+    <strong><p1>User Name :  </p1><br></strong> 
+        <i class="fa-solid fa-user" id="user-icon"></i>
+        <span>' . $_SESSION['user'] . '</span>
+        <div class="dropdown-content" id="user-dropdown">
+            <a href="#">Profile</a>
+            <a href="#">Logout</a>
+        </div>
+    </div>
+ ';
+
+} else {      
+            //Lucky add the code to instrust users to login and redirect users back to login page
+        }
+?>
+                 <a href="dashboard.php">
+                    <i class="fa-solid fa-table-columns"></i>
+                    <span class="nav-item">Dashboard</span>
+                </a>
+               <!-- <span class="tooltip">Dashboard</span> -->
+            </li>
+            <li>
+                <a href="useradmin.php">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="nav-item">Users</span>
+                </a>
+               <!-- <span class="tooltip">Users</span>-->
+            </li>
+            <li>
+                <a href="productadmin.php">
+                    <i class="fa-solid fa-couch"></i>
+                    <span class="nav-item">Products</span>
+                </a>
+               <!-- <span class="tooltip">Products</span>-->
+            </li>
+            <li>
+                <a href="orderadmin.php">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <span class="nav-item">Orders</span>
+                </a>
+               <!-- <span class="tooltip">Orders</span> -->
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa-solid fa-message"></i>
+                    <span class="nav-item">Messages</span>
+                </a>
+                               <!-- <span class="tooltip">Messages</span> -->
+            </li>
+        </ul>
+
+   
+    </section>
