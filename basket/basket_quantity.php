5865ddc9c5cb80d36dd7d1086f2e6c13ad12114b
Luke-H-220095075 <220095075@aston.ac.uk>
2024-03-19 18:58:03 +0000

    basket add/quantity fix

--- basket/basket_quantity.php
+++ basket/basket_quantity.php
@@ -1,14 +1,15 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    session_start();
     $productId = isset($_POST['product_id']) ? intval($_POST['product_id']) : 0;
     $change = isset($_POST['change']) ? intval($_POST['change']) : 0;
 
     include '../connect.php';
     $userId = $_SESSION['userID'];
-    $sql = "SELECT basketId FROM baskets WHERE userId = ". $userId ." AND currentUserBasket = 1" ;
-    $result = $db->query($sql);
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-    $basketId = $row['basketId'];
+//    $sql = "SELECT basketId FROM baskets WHERE userId = ". $userId ." AND currentUserBasket = 1" ;
+//    $result = $db->query($sql);
+//    $row = $result->fetch(PDO::FETCH_ASSOC);
+//    $basketId = $row['basketId'];
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -16,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmtUpdate = $db->prepare("
             UPDATE basketproducts
             SET quantity = quantity + :change
-            WHERE basketId = $basketId
+            WHERE basketId = (SELECT basketId FROM baskets WHERE userId = :user_id)
             AND productId = :product_id
         ");
         $stmtUpdate->bindParam(':user_id', $userId, PDO::PARAM_INT);
@@ -24,11 +25,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmtUpdate->bindParam(':change', $change, PDO::PARAM_INT);
         $stmtUpdate->execute();
 
-        $newQuantity = $db->query("SELECT quantity FROM basketproducts WHERE basketId = $basketId AND productId = $productId")->fetchColumn();
+        $newQuantity = $db->query("SELECT quantity FROM basketproducts WHERE basketId = (SELECT basketId FROM baskets WHERE userId = $userId) AND productId = $productId")->fetchColumn();
         if ($newQuantity === 0) {
             $stmtRemove = $db->prepare("
                 DELETE FROM basketproducts
-                WHERE basketId = $basketId
+                WHERE basketId = (SELECT basketId FROM baskets WHERE userId = :user_id)
                 AND productId = :product_id
             ");
             $stmtRemove->bindParam(':user_id', $userId, PDO::PARAM_INT);
@@ -41,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Error: " . $e->getMessage();
     }
 
-    $db = null;
+    $pdo = null;
 }
 
 ?>
