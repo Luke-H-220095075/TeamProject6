c2f8824e1ab1eacc7a7432f00a47104a51d19fef
LuckyMuse <luckymuse2001@gmail.com>
2024-03-19 15:54:52 +0000

    Created orderadmin.php
    Sidebar added.

---
+++ admin/orderadmin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    
+    <script src="https://kit.fontawesome.com/d4aa4c134e.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+
+<header>
+<span>Furniche <i class="fa-solid fa-bars" id="togglebtn"></i></span>
+<?php
+include '../connect.php';
+session_start();
+
+        ?>
+        </div>
+        </header>
+<body>
+<section id="sidebar">
+        <ul class="side-menu top">
+            <div class="logo">
+            </div>
+           
+            <li class="active">
+            <?php
+
+if (isset($_SESSION['user'])) {
+    echo '
+    <div class="user-info">
+    <strong><p1>User Name :  </p1><br></strong> 
+        <i class="fa-solid fa-user" id="user-icon"></i>
+        <span>' . $_SESSION['user'] . '</span>
+        <div class="dropdown-content" id="user-dropdown">
+            <a href="#">Profile</a>
+            <a href="#">Logout</a>
+        </div>
+    </div>
+ ';
+
+} else {
+          
+            //Lucky add the code to instrust users to login and redirect users back to login page
+     
+        }
+
+?>
