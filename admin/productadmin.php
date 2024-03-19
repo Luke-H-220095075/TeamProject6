9b8b1ffd84fa598d06726143c6212daccee850df
LuckyMuse <luckymuse2001@gmail.com>
2024-03-19 15:04:36 +0000

    Including Bootstrap
    Bootstrap that was required

--- admin/productadmin.php
+++ admin/productadmin.php
@@ -432,4 +432,15 @@ $totalPages = ceil($row['total'] / $limit);
 echo "No products found.";
 }
 ?>
+
 </div>
+
+
+    <!-- Include Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script src="script.js"></script>
+</body>
+</html>
