fc36d8cf70aa47ca1b9de65ec931392a0287efb4
LuckyMuse <luckymuse2001@gmail.com>
2024-03-19 14:42:01 +0000

    AdminDashBoard Header

---
+++ admin/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    
+    <script src="https://kit.fontawesome.com/d4aa4c134e.js" crossorigin="anonymous"></script>
+
+        <title>FurnicheDashboard</title>
+</head>
+
+<header>
+<span>Furniche <i class="fa-solid fa-bars" id="togglebtn"></i></span>
+
+<?php
+include '../connect.php';
+session_start();
