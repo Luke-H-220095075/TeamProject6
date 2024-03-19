34dbcb735721b7ba23b3ed5fce64fe899ba57809
LuckyMuse <luckymuse2001@gmail.com>
2024-03-19 14:43:20 +0000

    KPI Admin Dashboard
    This is the PHP required for the KPIs seen on the initial page.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -16,3 +16,22 @@
 <?php
 include '../connect.php';
 session_start();
+
+ //KPIs for the AdminDashboard
+ function getTotalCount($db, $table, $condition = "") {
+    $sql = "SELECT COUNT(*) as count FROM $table $condition";
+    $stmt = $db->query($sql);
+    
+    if ($stmt) {
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['count'];
+    } else {
+        return 0;
+    }
+}
+// KPI - Total Users
+$totalUsers = getTotalCount($db, "users");
+// KPI - Total orders
+$totalOrders = getTotalCount($db, "orders");
+// KPI - Pending Approvals
+$totalPending = getTotalCount($db, "orders", "WHERE `deliveryStatus` = 'Pending Approval'");
