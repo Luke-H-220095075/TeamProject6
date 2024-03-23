38b6ce1be2dc570052639ad2b85b4ee82bdee661
t00000mmmmm <tomgh4@yahoo.co.uk>
2024-03-23 23:07:28 +0000

    button fix

--- basket/basket.php
+++ basket/basket.php
@@ -222,6 +222,7 @@ if ($basketId != $mainbasketId) {
                 window.location.reload();
             }, 20);
         }
+        </script>
     </body>
     <footer class="footer">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
