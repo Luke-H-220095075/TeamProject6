7d43bb0d5a6f74b5f7c77008135538914ee9d959
Enson Wong <wongkm100@gmail.com>
2024-03-23 22:13:45 +0000

    Changes

--- checkout.php
+++ checkout.php
@@ -54,6 +54,7 @@ function purchase($db, $basketId)
       $db->query($sql);
       $sql = "INSERT INTO baskets (userId, currentUserBasket) VALUES (" . $_SESSION['userID'] . ", 1)";
       $db->query($sql);
+      $_SESSION["basketID"] = null;
       header('Location: history.php');
     }
   }
@@ -264,7 +265,6 @@ function purchase($db, $basketId)
   integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script>
 </body>
-</div>
 
 <footer class="footer">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
