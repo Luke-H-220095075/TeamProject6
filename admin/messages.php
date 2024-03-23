4d8b347b8795b799e25ed2b6b9774ec534c66a61
Isobel-Weaver <148205155+Isobel-Weaver@users.noreply.github.com>
2024-03-23 18:46:51 +0000

    some styling

--- admin/messages.php
+++ admin/messages.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/d4aa4c134e.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
         <title>User messages</title>
