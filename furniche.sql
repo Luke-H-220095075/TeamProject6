0fb00d7f0e10ba46b5f8c573fee461ecd51ae7af
t00000mmmmm <tomgh4@yahoo.co.uk>
2024-03-24 21:38:18 +0000

    database update
    reset counters added product reviews

--- furniche.sql
+++ furniche.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2024 at 03:28 PM
+-- Generation Time: Mar 24, 2024 at 10:37 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -38,18 +38,7 @@ CREATE TABLE `basketproducts` (
 --
 
 INSERT INTO `basketproducts` (`basketId`, `productId`, `quantity`) VALUES
-(1, 4, 4),
-(1, 5, 3),
-(1, 2, 1),
-(63, 3, 1),
-(64, 2, 1),
-(65, 1, 1),
-(66, 3, 1),
-(67, 1, 1),
-(68, 1, 1),
-(69, 3, 1),
-(70, 4, 1),
-(71, 2, 1);
+(1, 12, 1);
 
 -- --------------------------------------------------------
 
@@ -70,17 +59,8 @@ CREATE TABLE `baskets` (
 --
 
 INSERT INTO `baskets` (`basketId`, `userId`, `dateAdded`, `discountId`, `currentUserBasket`) VALUES
-(1, 1, '2024-02-07 12:22:40', 1, 0),
-(63, 1, '2024-03-24 11:51:39', NULL, 0),
-(64, 1, '2024-03-24 12:20:21', NULL, 0),
-(65, 1, '2024-03-24 12:25:58', NULL, 0),
-(66, 1, '2024-03-24 12:30:25', NULL, 0),
-(67, 1, '2024-03-24 12:31:25', NULL, 0),
-(68, 1, '2024-03-24 12:36:30', NULL, 0),
-(69, 1, '2024-03-24 12:37:32', NULL, 0),
-(70, 1, '2024-03-24 12:45:30', NULL, 0),
-(71, 1, '2024-03-24 12:48:59', NULL, 0),
-(72, 1, '2024-03-24 12:51:15', NULL, 1);
+(1, 1, '2024-03-24 18:38:05', NULL, 0),
+(2, 1, '2024-03-24 18:38:36', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -117,13 +97,6 @@ CREATE TABLE `inquiries` (
   `reply` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `inquiries`
---
-
-INSERT INTO `inquiries` (`inquiryId`, `userId`, `inquiryDate`, `subject`, `message`, `reply`) VALUES
-(1, 1, '2023-12-12', 'test', 'test', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -137,6 +110,14 @@ CREATE TABLE `orderreviews` (
   `rating` int(1) NOT NULL CHECK (`rating` >= 1 and `rating` <= 5),
   `description` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderreviews`
+--
+
+INSERT INTO `orderreviews` (`reviewId`, `orderId`, `reviewDate`, `rating`, `description`) VALUES
+(1, 1, '2024-03-24 18:39:00', 5, 'awesome stuff'),
+(2, 1, '2024-03-24 18:39:04', 5, 'awesome stuff');
 
 -- --------------------------------------------------------
 
@@ -160,9 +141,34 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`orderId`, `basketId`, `userId`, `dateAdded`, `deliveryOption`, `deliveryDate`, `deliveryStatus`, `notes`) VALUES
-(119, 69, 1, '2024-03-24 12:45:30', 'standard', NULL, NULL, ''),
-(120, 70, 1, '2024-03-24 12:48:59', 'standard', NULL, NULL, ''),
-(121, 71, 1, '2024-03-24 12:51:15', 'standard', NULL, NULL, '');
+(1, 1, 1, '2024-03-24 18:38:36', 'standard', NULL, NULL, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productreviews`
+--
+
+CREATE TABLE `productreviews` (
+  `userId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `reviewDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `rating` int(1) NOT NULL CHECK (`rating` >= 1 and `rating` <= 5),
+  `description` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `productreviews`
+--
+
+INSERT INTO `productreviews` (`userId`, `productId`, `reviewDate`, `rating`, `description`) VALUES
+(1, 12, '2024-03-24 21:29:22', 5, 'neat stuff'),
+(1, 12, '2024-03-24 21:29:37', 5, 'neat stuff'),
+(1, 2, '2024-03-24 21:30:36', 4, 'nice'),
+(1, 2, '2024-03-24 21:33:34', 4, 'nice'),
+(1, 2, '2024-03-24 21:34:10', 4, 'nice'),
+(1, 2, '2024-03-24 21:34:12', 4, 'nice'),
+(1, 2, '2024-03-24 21:35:01', 4, 'nice');
 
 -- --------------------------------------------------------
 
@@ -199,7 +205,7 @@ INSERT INTO `products` (`productId`, `productName`, `productDescription`, `price
 (9, 'Bohemian Sofa', 'Living room', '549.99', '2023-12-01 13:25:36', 11, 38, 'bohemian', 'sofa', 'bohemian_sofa.jpg'),
 (10, 'Tropical Chair', 'Kitchen', '169.99', '2023-12-01 13:25:36', 10, 32, 'tropical', 'chair', 'tropical_chair.jpg'),
 (11, 'Modern Sofa 2', 'Living room', '479.99', '2023-12-01 13:25:36', 8, 45, 'modern', 'sofa', 'modern_sofa_2.jpg'),
-(12, 'Minimal Desk 2', 'Office', '219.99', '2023-12-01 13:25:36', 6, 18, 'minimal', 'desk', 'minimal_desk_2.jpg'),
+(12, 'Minimal Desk 2', 'Office', '219.99', '2023-12-01 13:25:36', 7, 17, 'minimal', 'desk', 'minimal_desk_2.jpg'),
 (13, 'Rustic Chair 2', 'Bedroom', '149.99', '2023-12-01 13:25:36', 7, 32, 'rustic', 'chair', 'rustic_chair_2.jpg'),
 (14, 'Bohemian Bed 2', 'Double bedroom', '679.99', '2023-12-01 13:25:36', 10, 35, 'bohemian', 'bed', 'bohemian_bed_2.jpg'),
 (15, 'Tropical Wardrobe 2', 'Single bedroom', '279.99', '2023-12-01 13:25:36', 5, 28, 'tropical', 'wardrobe', 'tropical_wardrobe_2.jpg'),
@@ -234,7 +240,7 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`transactionId`, `orderId`, `amount`, `status`, `cardDetails`, `transactionDate`) VALUES
-(2, 121, '199.99', 'approved', '|000|00/00', '2024-03-24 12:51:15');
+(1, 1, '219.99', 'pending', '||', '2024-03-24 18:38:36');
 
 -- --------------------------------------------------------
 
@@ -264,10 +270,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `admin`, `firstname`, `surname`, `address`, `email`, `username`, `phone`, `password`, `dateCreated`, `secretAnswer`, `contactByEmail`, `contactByText`, `pendingApproval`) VALUES
-(1, 'admin', 'admin', 'admin', 'address', 'admin@admins', 'admin', '123456', '$2y$10$AcbFhwU3/CXVOVgY7..zvOxByK/tiVOB/b5jGVbahEzHOHRQdC9aq', '2024-02-07 11:51:07', '$2y$10$.CpVg5QHunYIiOpJECtS7.iU9w7lbPnaHd/eTlZOfamtxYZOKh5zO', 0, 0, 1),
-(2, 'admin', 'lucy', 'lucy', '33 lucy lane', 'luc@luc.ac.uk', 'lucy', '1010101', '$2y$10$Nyu9gtGpqtGuGm5ba5Us1ehd3E0cLbp5gISxBYmCPip9Z6Rh2VULe', '2024-03-19 13:13:47', '', 0, 0, 1),
-(3, 'admin', 'test', 'test', '22 Jump Street', 'email@email.com', 'test', '1212112', '$2y$10$u12r0JbNCCJVI5duBVY/.edBMmvUXQh6Xg3ICc6XTGrpVCp3AuGLO', '2024-03-19 13:14:12', '', 0, 0, 0),
-(4, 'admin', 'test', 'test', '22 Jump Street', 'email@email.com', 'test', '1212112', '$2y$10$ckMq8OPirQBTIGstfkY6geyXICfrYnk7cONtjmQ6tjcTLdBDT49kO', '2024-03-19 13:54:34', '', 0, 0, 0);
+(1, 'admin', 'john', 'doe', NULL, 'admin@furniche.com', 'admin', NULL, '$2y$10$pjwd1AtYTCB1Rl/ueiwWDu9dTlTLlTJIMn1qrim6/QEB9D3b4vbci', '2024-03-24 18:38:05', '$2y$10$d7G90/yF8aHQ0SSaT0dlYO1.XQSnuBm5yVzSQ8K8XczSqqB1qNp..', 0, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -343,7 +346,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `baskets`
 --
 ALTER TABLE `baskets`
-  MODIFY `basketId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `basketId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `discounts`
@@ -355,19 +358,19 @@ ALTER TABLE `discounts`
 -- AUTO_INCREMENT for table `inquiries`
 --
 ALTER TABLE `inquiries`
-  MODIFY `inquiryId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `inquiryId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orderreviews`
 --
 ALTER TABLE `orderreviews`
-  MODIFY `reviewId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `reviewId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -379,13 +382,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
