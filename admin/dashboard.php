abf24e2a2dfc111e7ebd489f2715ff2ad7b6cfd4
LuckyMuse <luckymuse2001@gmail.com>
2024-03-24 14:12:33 +0000

    Sidebars Updated

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -72,7 +72,67 @@ if (isset($_SESSION['user'])) {
      
         }
 
-?>
+?><li>
+<a href="dashboard.php">
+   <i class="fa-solid fa-table-columns"></i>
+   <span class="nav-item">Dashboard</span>
+</a>
+<!-- <span class="tooltip">Dashboard</span> -->
+</li>
+<li>
+<a href="useradmin.php">
+<i class="fa-solid fa-users"></i>
+   <span class="nav-item">Users</span>
+</a>
+<!-- <span class="tooltip">Users</span>-->
+</li>
+<li>
+<a href="productadmin.php">
+   <i class="fa-solid fa-couch"></i>
+   <span class="nav-item">Products</span>
+</a>
+<!-- <span class="tooltip">Products</span>-->
+</li>
+<li>
+<a href="orderadmin.php">
+   <i class="fa-solid fa-cart-shopping"></i>
+   <span class="nav-item">Orders</span>
+</a>
+<!-- <span class="tooltip">Orders</span> -->
+</li>
+<li>
+<a href="transactions.php">
+<i class="fa-solid fa-money-bill"></i>
+   <span class="nav-item">Transactions</span>
+</a>
+
+</li>
+<li>
+<a href="messages.php">
+   <i class="fa-solid fa-message"></i>
+   <span class="nav-item">Messages</span>
+</a>
+              <!-- <span class="tooltip">Messages</span> -->
+</li>
+<li>
+
+<li>
+<a href="pendingrequests.php">
+   <i class="fa-solid fa-user"></i>
+   <span class="nav-item">Admin Requests</span>
+</a>
+<!-- <span class="tooltip">Users</span>-->
+</li>
+<li>
+<a href="../index.php">
+   <i class="fa-solid fa-star"></i>
+   <span class="nav-item">View As User</span>
+</a>
+              <!-- <span class="tooltip">mainpage</span> -->
+
+</li>
+</ul>
+
                 <a href="dashboard.php">
                     <i class="fa-solid fa-table-columns"></i>
                     <span class="nav-item">Dashboard</span>
@@ -114,6 +174,13 @@ if (isset($_SESSION['user'])) {
                     <span class="nav-item">Messages</span>
                 </a>
                                <!-- <span class="tooltip">Messages</span> -->
+
+            </li>
+
+            <a href="transactions.php">
+                    <i class="fa-solid fa-message"></i>
+                    <span class="nav-item">Transactions</span>
+                </a>
 
             </li>
             <li>
