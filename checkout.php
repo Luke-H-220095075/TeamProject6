c1d7c8a3f74d21f186861aec19330857296b1424
Gcreader <96251116+Gcreader@users.noreply.github.com>
2024-03-24 15:34:20 +0000

    nav bar changes

--- checkout.php
+++ checkout.php
@@ -150,10 +150,6 @@ function purchase($db, $basketId, $basketcost)
               }
               ?>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </nav>
