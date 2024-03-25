50d5ce0ce1c236f6ed9150ce35e4391d0d592c72
LuckyMuse <luckymuse2001@gmail.com>
2024-03-25 11:15:02 +0000

    Returnadmin.php

--- admin/returnsadmin.php
+++ admin/returnsadmin.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
     <script src="https://kit.fontawesome.com/d4aa4c134e.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+
+        <title>FurnicheDashboard</title>
+</head>   
+
+
 
 <header>
-<span>Furniche <i class="fa-solid fa-bars" id="togglebtn"></i></span>
-<?php
-include '../connect.php';
-session_start();
-
-        ?>
+<a href="../index.php" style="color: inherit; text-decoration: none">Furniche </a><i class="fa-solid fa-bars" id="togglebtn"></i>
+      
         </div>
         </header>
 <body>
-<section id="sidebar">
+    <section id="sidebar">
         <ul class="side-menu top">
             <div class="logo">
             </div>
@@ -29,57 +30,47 @@ session_start();
 if (isset($_SESSION['user'])) {
     echo '
     <div class="user-info">
-    <strong><p1>User Name :  </p1><br></strong> 
+    <strong><p1>User Name :  </p1><br></strong> &emsp;
         <i class="fa-solid fa-user" id="user-icon"></i>
         <span>' . $_SESSION['user'] . '</span>
-        <div class="dropdown-content" id="user-dropdown">
-            <a href="#">Profile</a>
-            <a href="#">Logout</a>
-        </div>
+        
     </div>
  ';
 
 } else {
           
-            //Lucky add the code to instrust users to login and redirect users back to login page
+   
      
         }
 
 ?>
-<li>
+              <li>
                  <a href="dashboard.php">
                     <i class="fa-solid fa-table-columns"></i>
                     <span class="nav-item">Dashboard</span>
                 </a>
-               <!-- <span class="tooltip">Dashboard</span> -->
+               
             </li>
             <li>
                 <a href="useradmin.php">
                 <i class="fa-solid fa-users"></i>
                     <span class="nav-item">Users</span>
                 </a>
-               <!-- <span class="tooltip">Users</span>-->
+               
             </li>
             <li>
                 <a href="productadmin.php">
                     <i class="fa-solid fa-couch"></i>
                     <span class="nav-item">Products</span>
                 </a>
-               <!-- <span class="tooltip">Products</span>-->
+               
             </li>
             <li>
                 <a href="orderadmin.php">
                     <i class="fa-solid fa-cart-shopping"></i>
                     <span class="nav-item">Orders</span>
                 </a>
-               <!-- <span class="tooltip">Orders</span> -->
-            </li>
-            <li>
-                <a href="returnsadmin.php">
-                    <i class="fa-solid fa-box"></i>
-                    <span class="nav-item">Returns</span>
-                </a>
-               <!-- <span class="tooltip">Orders</span> -->
+               
             </li>
             <li>
             <a href="transactions.php">
@@ -89,11 +80,17 @@ if (isset($_SESSION['user'])) {
 
             </li>
             <li>
+                <a href="returnsadmin.php">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="nav-item">Return Requests</span>
+                </a>
+              
+            </li>
+            <li>
                 <a href="messages.php">
                     <i class="fa-solid fa-message"></i>
                     <span class="nav-item">Messages</span>
                 </a>
-                               <!-- <span class="tooltip">Messages</span> -->
             </li>
             <li>
           
@@ -102,14 +99,12 @@ if (isset($_SESSION['user'])) {
                     <i class="fa-solid fa-user"></i>
                     <span class="nav-item">Admin Requests</span>
                 </a>
-               <!-- <span class="tooltip">Users</span>-->
             </li>
             <li>
                 <a href="../index.php">
                     <i class="fa-solid fa-star"></i>
                     <span class="nav-item">View As User</span>
                 </a>
-                               <!-- <span class="tooltip">mainpage</span> -->
 
             </li>
         </ul>
@@ -117,194 +112,185 @@ if (isset($_SESSION['user'])) {
 
    
     </section>
-    <br><br><h1> Returns Admin Dashboard</h1> 
 
-    
+ <main>
+    <br><br>
+    <h1>Pending Returns</h1>
 
     <?php
-include '../connect.php';
+    include '../connect.php';
 
-function fetchRecentOrders($db) {
-    try {
-        $sql = "SELECT * FROM orders ORDER BY dateAdded DESC LIMIT 10";
-        $stmt = $db->query($sql);
-        
-        if ($stmt) {
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } else {
-            return false;
-        }
-    } catch (PDOException $ex) {
-        echo "Error fetching recent orders: " . $ex->getMessage();
-        return false;
+    // Retrieve returns from the database
+    $sql = "SELECT * FROM itemreturns";
+    $stmt = $db->query($sql);
+    $returns = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Filter returns by Status
+    $statusFilter = isset($_GET['status']) ? $_GET['status'] : null;
+    $sql = "SELECT * FROM itemreturns";
+
+    if ($statusFilter !== null) {
+        $sql .= " WHERE status = :status";
     }
-}
-
-
-//Change colour of Delivery Status
-
-function getStatusClass($returnStatus) {
-    switch ($returnStatus) {
-        case 'Returned':
-            return 'status-returned';
-        case 'Currently returning':
-            return 'status-returning';
-        case 'Dispatching':
-            return 'status-dispatching';
-        case 'Pending Approval':
-            return 'status-pending';
-        default:
-            return '';
-    }
-}
-
-
-$statusFilter = isset($_GET['returnStatus']) ? $_GET['deliveryStatus'] : null;
-
-$sql = "SELECT * FROM orders";
-
-if ($statusFilter !== null) {
-$sql .= " WHERE returnStatus = :returnStatus";
-}
-
-$limit = 8;
-$page = isset($_GET['page']) ? $_GET['page'] : 1;
-$offset = ($page - 1) * $limit;
-$sql .= " LIMIT :limit OFFSET :offset";
-
-$stmt = $db->prepare($sql);
-$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
-$stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
-if ($statusFilter !== null) {
-$stmt->bindParam(':returnStatus', $statusFilter, PDO::PARAM_STR);
-}
-$stmt->execute();
-$orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-if ($orders && $stmt->rowCount() > 0) {
-foreach ($orders as $order) {
-}
-
-} else {
-echo "<tr><td colspan='8'>No orders found.</td></tr>";
-}
-
-
-// Search functionality
-if (isset($_GET['search']) && !empty($_GET['search'])) {
-    $searchTerm = $_GET['search'];
-    $sql = "SELECT * FROM orders WHERE orderId LIKE :searchTerm
-           OR basketId LIKE :searchTerm 
-           OR userId LIKE :searchTerm 
-           OR `deliveryStatus` LIKE :searchTerm 
-           OR deliveryOption LIKE :searchTerm 
-           OR deliveryDate LIKE :searchTerm 
-           OR notes LIKE :searchTerm";
 
     $stmt = $db->prepare($sql);
-    $stmt->bindValue(':searchTerm', "%$searchTerm%", PDO::PARAM_STR);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if ($result) {
-        foreach ($result as $row) {
-        }
-    } else {
-        echo "";
+    if ($statusFilter !== null) {
+        $stmt->bindParam(':status', $statusFilter, PDO::PARAM_STR);
     }
-}
 
-?>
+    $stmt->execute();
 
+    $returns = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
-
-<!--Return View Table -->
-
-<!--Header of Table, BOOTSTRAP WAS USED -->
-<div class="info-table">
-<div class="table-header" style="background-color: #e2b489; padding-top: 10px;">
-    <div class="container custom-background">
-    <br><br> <strong><h3>Returns</h3></strong> </strong> &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp;
+    //Updates Return Status
+    if(isset($_POST['approve'])) {
+        $returnId = $_POST['returnId'];
         
-        <div class="row justify-content-end align-items-right">
-            <div class="col-md-6">
-                <form method="GET" action="">
-                    <div class="input-group mb-3 custom-search-bar">
-                        <input type="text" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="search-button" name="search">
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary" type="submit" id="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
+        $sql = "UPDATE itemreturns SET status = 'returning' WHERE returnId = :returnId";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':returnId', $returnId);
+        $stmt->execute();
+        
+        header('Location: returnsadmin.php');
+        exit;
+    }
+    
+    if(isset($_POST['deny'])) {
+        $returnId = $_POST['returnId'];
+        
+        $sql = "UPDATE itemreturns SET status = 'denied' WHERE returnId = :returnId";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':returnId', $returnId);
+        $stmt->execute();
+        
+        header('Location: returnsadmin.php');
+        exit;
+    }
+
+    // Checks if there are returns
+    if ($returns) {
+    ?>
+
+    <div class="info-table">
+
+        <div class="table-header" style="background-color: #e2b489; padding-top: 10px;">
+            <div class="container custom-background">
+                <strong><h3>Returns</h3></strong> &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp;
+
+                <div class="row justify-content-end align-items-right">
+
+                    <div class="col-md-1">
+                        <div class="input-group mb-3 custom-dropdown-toggle">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="status" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">status</button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="returnsadmin.php?status=pending">Pending</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=returning">Returning</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=returned">Returned</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=denied">Denied</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="col-md-3">
-            <div class="input-group mb-3 custom-dropdown-toggle">
-            <div class="input-group-prepend">
-             <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="deliveryDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Delivery Status</button>
-             <div class="dropdown-menu">
-            <a class="dropdown-item" href="orderadmin.php?deliveryStatus=Delivered">Returned</a>
-            <a class="dropdown-item" href="orderadmin.php?deliveryStatus=Currently%20Delivering">Currently Returning</a>
-            <a class="dropdown-item" href="orderadmin.php?deliveryStatus=Dispatching">Dispatching</a>
-            <a class="dropdown-item" href="orderadmin.php?deliveryStatus=Pending%20Approval">Pending Approval</a>
-        </div>
-    </div>
-</div>
-
+                </div>
             </div>
         </div>
-    </div>
-</div>
+    
 
-
-<table>
-    <thead>
-        <tr>
-            <th>Order ID</th>
-            <th>Basket ID</th>
-            <th>User ID</th>
-            <th>Date Added</th>
-            <th>Delivery Option</th>
-            <th>Delivery Status</th>
-            <th>Delivery Date</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-
-    <!--PHP TO DISPLAY DATA FROM DATABASE-->
-        <?php foreach ($orders as $order) { ?>
+    <table>
+        <thead>
             <tr>
-                <td><?php echo $order['orderId']; ?></td>
-                <td><?php echo $order['basketId']; ?></td>
-                <td><?php echo $order['userId']; ?></td>
-                <td><?php echo $order['dateAdded']; ?></td>
-                <td><?php echo $order['deliveryOption']; ?></td>
-                <td class="<?php echo getStatusClass($order['deliveryStatus']); ?>"><?php echo $order['deliveryStatus']; ?></td>
-                  <td><?php echo $order['deliveryDate']; ?></td>
-                <td>
-                <span style='margin-right: 5px;'></span>
-                <a href="editorder.php?orderId=<?php echo $order['orderId']; ?>">
-                        <i class="fa-solid fa-eye"></i>
-                    </a>
-                    <span style='margin-right: 5px;'></span>
-                    <a href="editorder.php?orderId=<?php echo $order['orderId']; ?>">
-                        <i class="fa-solid fa-pencil"></i>
-                    </a>
-                </td>
+                <th>ID</th>
+                <th>Product ID</th>
+                <th>Order ID</th>
+                <th>Status</th>
+                <th>Explanation</th>
+                <th>Price</th>
+                <th>Action</th>
             </tr>
-        <?php } ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($returns as $return) { ?>
+                <tr>
+                    <td><?php echo $return['returnId']; ?></td>
+                    <td><?php echo $return['productId']; ?></td>
+                    <td><?php echo $return['orderID']; ?></td>
+                    <td><?php echo $return['status']; ?></td>
+                    <td><?php echo $return['explanation']; ?></td>
+                    <td><?php echo $return['price']; ?></td>
+                    <td>
+                        <form action="returnsadmin.php" method="POST">
+                            <input type="hidden" name="returnId" value="<?php echo $return['returnId']; ?>">
+                            <button type="submit" name="approve" class="btn btn-primary">Approve</button>
+                        </form>
+
+                        <form action="returnsadmin.php" method="POST">
+                            <input type="hidden" name="returnId" value="<?php echo $return['returnId']; ?>">
+                            <button type="submit" name="deny" class="btn btn-primary">Denied</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
+</div>
+<?php
+} else {
+    echo '
+    <div class="info-table">
 
+        <div class="table-header" style="background-color: #e2b489; padding-top: 10px;">
+            <div class="container custom-background">
+                <strong><h3>Returns</h3></strong> &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp; &emsp;&ensp;
 
+                <div class="row justify-content-end align-items-right">
 
+                    <div class="col-md-1">
+                        <div class="input-group mb-3 custom-dropdown-toggle">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="status" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">status</button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="returnsadmin.php?status=pending">Pending</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=returning">Returning</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=returned">Returned</a>
+                                    <a class="dropdown-item" href="returnsadmin.php?status=denied">Denied</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
 
-    <script src="script.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
- </body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Product ID</th>
+                <th>Order ID</th>
+                <th>Status</th>
+                <th>Explanation</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        </table>
+       
+        <p1>No returns found</p1>
+        
+        </div>'
+        ;
+        
+}
+?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
+</body>
+
 </html>
